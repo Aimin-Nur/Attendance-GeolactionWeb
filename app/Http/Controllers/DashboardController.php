e1cac361738081b59a0fe1a0ba1623de5198f71b
muhaimin nur <you@example.com>
2023-09-19 11:19:54 +0800

    notifikasi di halaman dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Absen;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.dashboard');
+        $today = date("Y-m-d");
+        $nip = Auth::guard('karyawan')->user()->NIP;
+        $presensiToday = DB::table('absen')->where('tgl_absen',$today)->where('NIP',$nip)->first();
+        return view('dashboard.dashboard', compact('presensiToday'));
     }
 }
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -73,7 +73,7 @@
                                     </div>
                                     <div class="presencedetail">
                                         <h4 class="presencetitle">Masuk</h4>
-                                        <span>07:00</span>
+                                        <span>{{ $presensiToday != null ? $presensiToday->jam_in : 'Belum Absen'}}</span>
                                     </div>
                                 </div>
                             </div>
@@ -88,7 +88,7 @@
                                     </div>
                                     <div class="presencedetail">
                                         <h4 class="presencetitle">Pulang</h4>
-                                        <span>12:00</span>
+                                        <span>{{ $presensiToday != null && $presensiToday->jam_out != null ? $presensiToday->jam_out : 'Belum Absen'}}</span>
                                     </div>
                                 </div>
                             </div>
@@ -97,73 +97,6 @@
                 </div>
             </div>
 
-            <div class="rekappresence">
-                <div id="chartdiv"></div>
-                <!-- <div class="row">
-                    <div class="col-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="presencecontent">
-                                    <div class="iconpresence primary">
-                                        <ion-icon name="log-in"></ion-icon>
-                                    </div>
-                                    <div class="presencedetail">
-                                        <h4 class="rekappresencetitle">Hadir</h4>
-                                        <span class="rekappresencedetail">0 Hari</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="presencecontent">
-                                    <div class="iconpresence green">
-                                        <ion-icon name="document-text"></ion-icon>
-                                    </div>
-                                    <div class="presencedetail">
-                                        <h4 class="rekappresencetitle">Izin</h4>
-                                        <span class="rekappresencedetail">0 Hari</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row mt-1">
-                    <div class="col-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="presencecontent">
-                                    <div class="iconpresence warning">
-                                        <ion-icon name="sad"></ion-icon>
-                                    </div>
-                                    <div class="presencedetail">
-                                        <h4 class="rekappresencetitle">Sakit</h4>
-                                        <span class="rekappresencedetail">0 Hari</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="presencecontent">
-                                    <div class="iconpresence danger">
-                                        <ion-icon name="alarm"></ion-icon>
-                                    </div>
-                                    <div class="presencedetail">
-                                        <h4 class="rekappresencetitle">Terlambat</h4>
-                                        <span class="rekappresencedetail">0 Hari</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
-            </div>
             <div class="presencetab mt-2">
                 <div class="tab-pane fade show active" id="pilled" role="tabpanel">
                     <ul class="nav nav-tabs style1" role="tablist">
